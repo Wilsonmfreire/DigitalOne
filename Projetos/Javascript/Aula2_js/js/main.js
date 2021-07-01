3abee46c68648d0bcdef43c5fa436cb1be7856f1
Wilsonmfreire <62375886+Wilsonmfreire@users.noreply.github.com>
2021-07-01 12:18:27 -0300

    trabalhando com condicionais

--- Projetos/Javascript/Aula2_js/js/main.js
+++ Projetos/Javascript/Aula2_js/js/main.js
@@ -1,4 +1,37 @@
-var nome = "Wilson Freire";
+
+
+/*condicionai if / else
+//prompt exibi uma pergunta para o usuario na tela.
+var idade = prompt('Qual sua idade?');
+if (idade >=18){
+    alert('maior de idade');
+}else{
+    alert('menor de idade');
+}*/
+
+
+/* lista de objetos
+var frutas = [{nome: 'maça', cor: 'vermelha'}, {nome: 'uva', cor: 'roxa'}]
+console.log(frutas);
+alert(frutas[1].nome);*/
+
+/*arrays
+var lista = ['maça', 'pera', 'laranja'];
+//push para adicionar
+lista.push('uva');
+// para retirar o ultimo
+lista.pop();
+//para ver o tamanho da lista
+lista.length;
+console.log(lista);
+//passando a lista para string
+console.log(lista.toString());
+//colocando elementos na lista;
+console.log(lista.join(" "));*/
+
+
+//introdução ao js
+/*var nome = "Wilson Freire";
 var idade = 32;
 var idade2 = 33;
 var frase = 'Japão é o melhor time do mundo'
@@ -11,4 +44,4 @@ console.log(frase.replace ('Brasil é o melhor time do mundo'));
 //alert(frase.replace('Japão', 'Brasil'));
 //toUpperCase transforma minusculo em maiusculo.
 //console.log(frase.toUpperCase());
-console.log(frase.toLowerCase());
+console.log(frase.toLowerCase());*/
