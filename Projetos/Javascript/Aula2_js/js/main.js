db92ec2f67ef8c7426aa8dc569cdba86daded6b7
Wilsonmfreire <62375886+Wilsonmfreire@users.noreply.github.com>
2021-07-01 11:52:25 -0300

    organizando pastas

---
+++ Projetos/Javascript/Aula2_js/js/main.js
@@ -0,0 +1,14 @@
+var nome = "Wilson Freire";
+var idade = 32;
+var idade2 = 33;
+var frase = 'Japão é o melhor time do mundo'
+//alert(nome + ' tem ' + idade);
+//alert (idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+//replace podemos alterar o valor da variavel passada a ele
+console.log(frase.replace ('Brasil é o melhor time do mundo'));
+//alert(frase.replace('Japão', 'Brasil'));
+//toUpperCase transforma minusculo em maiusculo.
+//console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
